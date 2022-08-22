b370077bcaf57e387616b37c92df19fcea33201b
veronicaglh <veronicagt.IT@gmail.com>
2022-08-22 09:44:26 +0300

    add details to UI screen

--- pythonGraph.py
+++ pythonGraph.py
@@ -26,48 +26,35 @@ myWindow.title("Computer Graphics")
 myWindow.geometry('563x789')
 myWindow['background'] = '#BE8C5B'
 
+load = PIL.Image.open('Image\\tkinterBackground.jpg')
+render = ImageTk.PhotoImage(load)
+img = Label(myWindow, image=render)
+img.place(x=0, y=0)
 
-def init():
-    pygame.init()
-    display = (500, 500)
-    pygame.display.set_mode(display, DOUBLEBUF | OPENGL)
-    glClearColor(0.0, 0.0, 0.0, 1.0)
-    gluOrtho2D(-1.0, 1.0, -1.0, 1.0)
+title1 = Label(myWindow, text="Welcome to Graphopedia", bg="#F5C5B9", font="Calibri 20 bold")
+title2 = Label(myWindow, text="Where you can see the graph of two functions", bg="#F5C5B9", font="Calibri 10 italic")
 
+title3a = Label(myWindow, text="Please chose one of the following pairs", bg="#F5C5B9", font="Calibri 10 italic")
+title3b = Label(myWindow, text="of functions to be drawn:", bg="#F5C5B9", font="Calibri 10 italic")
 
-def draw():
-    glClear(GL_COLOR_BUFFER_BIT)
-    glColor3f(1.0, 0.0, 0.0)
-    # generate 100 points within -1 to 1 range
-    x = np.linspace(-1, 1, 100)
-    y = np.sin(x)
-    z = np.cos(x)
-    glPointSize(10)
-    glBegin(GL_LINE_STRIP)
-    # for every pair (a, b) of the numbers in x, y
-    for a, b in zip(x, y):
-    # give (a, b) to OpenGL to draw
-        glVertex2f(a, b)
-        glColor3f(1.0, 0.0, 1.0)
+title4a = Label(myWindow, text="A. y=sin(x) and z=cos(x)", bg="#F5C5B9", font="Calibri 10 bold")
+title4b = Label(myWindow, text="B. y=x^3 and z=x^4", bg="#F5C5B9", font="Calibri 10 bold")
+title4c = Label(myWindow, text="C.y=x^2 and z=tan(x)", bg="#F5C5B9", font="Calibri 10 bold")
 
-    for c, d in zip(x, z):
-        glVertex2f(c, d)
-        glColor3f(1.0, 1.0, 0.0)
-    glEnd()
-    glFlush()
+title5a = Label(myWindow, text="In the input field below enter either A or B or C", bg="#F5C5B9", font="Calibri 10 italic")
+title5b = Label(myWindow, text="**A, B or C must be capital and have no other punctuation", bg="#F5C5B9", font="Calibri 8 italic")
+title5c = Label(myWindow, text="**The y func will be purple and the z func will be yellow ", bg="#F5C5B9", font="Calibri 8 italic")
 
+title1.place(x=110, y=180)
+title2.place(x=130, y=220)
+title3a.place(x=90, y=300)
+title3b.place(x=90, y=320)
+title4a.place(x=100, y=355)
+title4b.place(x=100, y=375)
+title4c.place(x=100, y=395)
+title5a.place(x=90, y=430)
+title5b.place(x=90, y=450)
+title5c.place(x=90, y=470)
 
-def main():
-    init()
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                quit()
-
-        draw()
-        pygame.display.flip()
-        pygame.time.wait(10)
-main()
 
 myWindow.mainloop()
